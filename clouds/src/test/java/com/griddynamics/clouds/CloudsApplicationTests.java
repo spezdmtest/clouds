f17bb0fb7267934080e3615d7468cce06fbecdf7
spezdmtest <spezd.m.@gmail.com>
2023-01-02 10:03:38 +0200

    Cloud

---
+++ clouds/src/test/java/com/griddynamics/clouds/CloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.clouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
